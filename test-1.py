298fe56024dcafc1043acd3f19258852c1e36740
sayumujina <imestellia@gmail.com>
2024-10-01 09:24:09 +0700

    test 2

--- test-1.py
+++ test-1.py
@@ -2,3 +2,4 @@ first = int(input("Enter the first number:"))
 second = int(input("Enter the second number:"))
 
 print("The sum of these are: ", first + second)
+print("The product of these are", first * second)
